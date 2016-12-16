6312e18eef9d8fe95413e6d94f863e1bb410191a
Poornima M <poornima.malepati@go-jek.com>
2016-12-16 16:10:55 +0530

    Sending metrics to datadog

--- src/main/java/com/gojek/esb/consumer/LogConsumer.java
+++ src/main/java/com/gojek/esb/consumer/LogConsumer.java
@@ -1,32 +1,49 @@
 package com.gojek.esb.consumer;
 
 import com.gojek.esb.client.GenericHTTPClient;
+import com.gojek.esb.util.Clock;
+import com.timgroup.statsd.StatsDClient;
+import lombok.AllArgsConstructor;
 import org.apache.http.HttpResponse;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
+import java.time.Duration;
+import java.time.Instant;
 import java.util.List;
 
+@AllArgsConstructor
 public class LogConsumer {
 
     private static final Logger logger = LoggerFactory.getLogger(LogConsumer.class);
 
-    private EsbGenericConsumer consumer;
-    private GenericHTTPClient genericHTTPClient;
+    private final EsbGenericConsumer consumer;
+    private final GenericHTTPClient genericHTTPClient;
+    private final StatsDClient statsDClient;
 
-    public LogConsumer(EsbGenericConsumer consumer, GenericHTTPClient genericHTTPClient) {
-        this.consumer = consumer;
-        this.genericHTTPClient = genericHTTPClient;
-    }
+    private final Clock clock;
 
     public void processPartitions() throws IOException {
-        List<EsbMessage> messages = consumer.readMessages();
-        if (!messages.isEmpty()) {
-            HttpResponse resp = genericHTTPClient.execute(messages);
-            logger.info("Execution successful for {} records", messages.size());
+        Instant beforeCall = clock.now();
+        String batchReceivedCounter = "messages.received";
+        String batchSize = "messages.batch.size";
 
-            consumer.commitAsync();
+        try {
+            List<EsbMessage> messages = consumer.readMessages();
+            statsDClient.count(batchReceivedCounter, messages.size());
+            statsDClient.gauge(batchSize, messages.size());
+
+            if (!messages.isEmpty()) {
+                HttpResponse resp = genericHTTPClient.execute(messages);
+                logger.info("Execution successful for {} records", messages.size());
+
+                consumer.commitAsync();
+            }
+        } finally {
+            String timeTakenKey = "messages.process_partitions_time";
+            Instant afterCall = clock.now();
+            statsDClient.recordExecutionTime(timeTakenKey, Duration.between(beforeCall, afterCall).toMillis());
         }
     }
 }
