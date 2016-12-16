ac6b46c6cad6c4bc70e7ff45a2471b33fa5d540e
Poornima M <poornima.malepati@go-jek.com>
2016-12-16 16:10:55 +0530

    Sending metrics to datadog

--- src/test/java/com/gojek/esb/consumer/LogConsumerTest.java
+++ src/test/java/com/gojek/esb/consumer/LogConsumerTest.java
@@ -1,6 +1,8 @@
 package com.gojek.esb.consumer;
 
+import com.gojek.esb.util.Clock;
 import com.gojek.esb.client.GenericHTTPClient;
+import com.timgroup.statsd.StatsDClient;
 import org.apache.http.HttpResponse;
 import org.junit.Before;
 import org.junit.Test;
@@ -9,6 +11,8 @@ import org.mockito.Mock;
 import org.mockito.runners.MockitoJUnitRunner;
 
 import java.io.IOException;
+import java.time.Duration;
+import java.time.Instant;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -30,22 +34,30 @@ public class LogConsumerTest {
     @Mock
     private HttpResponse httpResponse;
 
+    @Mock
+    private StatsDClient statsDClient;
+
+    @Mock
+    private Clock clock;
+
     private LogConsumer logConsumer;
     private List<EsbMessage> messages;
 
     @Before
     public void setUp() throws Exception {
-        EsbMessage msg = new EsbMessage(new byte[]{}, new byte[]{}, "topic");
-        messages = Arrays.asList(msg);
+        EsbMessage msg1 = new EsbMessage(new byte[]{}, new byte[]{}, "topic");
+        EsbMessage msg2 = new EsbMessage(new byte[]{}, new byte[]{}, "topic");
+        messages = Arrays.asList(msg1, msg2);
 
+        logConsumer = new LogConsumer(esbGenericConsumer, genericHTTPClient, statsDClient, clock);
+
+        when(esbGenericConsumer.readMessages()).thenReturn(messages);
         when(genericHTTPClient.execute(any(List.class))).thenReturn(httpResponse);
+        when(clock.now()).thenReturn(Instant.now());
     }
 
     @Test
     public void shouldProcessPartitions() throws IOException {
-        when(esbGenericConsumer.readMessages()).thenReturn(messages);
-        logConsumer = new LogConsumer(esbGenericConsumer, genericHTTPClient);
-
         logConsumer.processPartitions();
 
         verify(genericHTTPClient).execute(messages);
@@ -54,10 +66,30 @@ public class LogConsumerTest {
     @Test
     public void shouldProcessEmptyPartitions() throws IOException {
         when(esbGenericConsumer.readMessages()).thenReturn(new ArrayList<>());
-        logConsumer = new LogConsumer(esbGenericConsumer, genericHTTPClient);
 
         logConsumer.processPartitions();
 
         verify(genericHTTPClient, times(0)).execute(any(List.class));
+    }
+
+    @Test
+    public void shouldSendNoOfMessagesReceivedCount() throws IOException {
+        logConsumer.processPartitions();
+        verify(statsDClient).count("messages.received", 2);
+    }
+
+    @Test
+    public void shouldSendPartitionProcessingTime() throws IOException {
+        Instant beforeCall = Instant.now();
+        Instant afterCall = beforeCall.plusSeconds(1);
+        when(clock.now()).thenReturn(beforeCall).thenReturn(afterCall);
+        logConsumer.processPartitions();
+        verify(statsDClient).recordExecutionTime("messages.process_partitions_time", Duration.between(beforeCall, afterCall).toMillis());
+    }
+
+    @Test
+    public void shouldSendBatchSize() throws IOException {
+        logConsumer.processPartitions();
+        verify(statsDClient).gauge("messages.batch.size", 2);
     }
 }
