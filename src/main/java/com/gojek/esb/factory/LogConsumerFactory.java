e4c97560941abe9e59ae5b3bd17551e6bf5571f0
Dinesh Kumar <dinesh.kumar@go-jek.com>
2017-01-09 15:21:53 +0530

    Upgraded consumer lib for sending audits while reading Messages

--- src/main/java/com/gojek/esb/factory/LogConsumerFactory.java
+++ src/main/java/com/gojek/esb/factory/LogConsumerFactory.java
@@ -1,25 +1,35 @@
 package com.gojek.esb.factory;
 
+import com.gojek.esb.audit.AuditMessageBuilder;
+import com.gojek.esb.config.ApplicationConfiguration;
+import com.gojek.esb.config.AuditConfig;
 import com.gojek.esb.config.KafkaConsumerConfig;
 import com.gojek.esb.consumer.EsbGenericConsumer;
 import com.gojek.esb.consumer.LogConsumer;
+import com.gojek.esb.server.AuditServiceResponseHandler;
+import com.gojek.esb.util.TimeUtil;
+import org.asynchttpclient.DefaultAsyncHttpClient;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.util.Optional;
 import java.util.regex.Pattern;
 
 public class LogConsumerFactory {
 
-    private static final Logger logger = LoggerFactory.getLogger(LogConsumerFactory.class.getName());
+    private static final ApplicationConfiguration appConfig = FactoryUtils.appConfig;
 
-    private static final KafkaConsumerConfig kafkaConsumerConfig = new KafkaConsumerConfig(FactoryUtils.appConfig.getKafkaAddress(),
+    private static final KafkaConsumerConfig kafkaConsumerConfig = new KafkaConsumerConfig(appConfig.getKafkaAddress(),
             FactoryUtils.appConfig.getConsumerGroupId(),
             Pattern.compile(FactoryUtils.appConfig.getKafkaTopic()),
             Long.MAX_VALUE,
             System.getenv()
     );
 
-    private static final EsbGenericConsumer genericConsumer = new GenericKafkaFactory().createConsumer(kafkaConsumerConfig);
+    private static final AuditConfig auditConfig = new AuditConfig(new DefaultAsyncHttpClient(), kafkaConsumerConfig.getGroupId(),
+            appConfig.getAuditServiceUrl(), appConfig.isAuditEnabled(), Optional.of(new AuditServiceResponseHandler()), Optional.of(new AuditMessageBuilder(new TimeUtil())));
+
+    private static final EsbGenericConsumer genericConsumer = new GenericKafkaFactory().createConsumer(kafkaConsumerConfig, auditConfig);
 
     public static LogConsumer getLogConsumer() {
         return new LogConsumer(genericConsumer, FactoryUtils.httpClient, FactoryUtils.statsDClient, FactoryUtils.clockInstance);
