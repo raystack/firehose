526fa55225867ec627ce97a9e486c11d91522fe6
Deepak Marathe <deepak.marathe@go-jek.com>
2017-02-01 16:49:06 +0530

    adding instantiation logic for dbsink in LogConsumerFactory. adding integration test.

--- src/main/java/com/gojek/esb/factory/LogConsumerFactory.java
+++ src/main/java/com/gojek/esb/factory/LogConsumerFactory.java
@@ -9,9 +9,17 @@ import com.gojek.esb.config.SinkType;
 import com.gojek.esb.consumer.EsbGenericConsumer;
 import com.gojek.esb.consumer.LogConsumer;
 import com.gojek.esb.server.AuditServiceResponseHandler;
-import com.gojek.esb.sink.DBSink;
+import com.gojek.esb.sink.BackOffProvider;
+import com.gojek.esb.sink.ExponentialBackOffProvider;
 import com.gojek.esb.sink.HttpSink;
+import com.gojek.esb.sink.ProtoToTableMapper;
+import com.gojek.esb.sink.QueryTemplate;
+import com.gojek.esb.sink.RetrySinkCommand;
 import com.gojek.esb.sink.Sink;
+import com.gojek.esb.sink.db.DBBatchCommand;
+import com.gojek.esb.sink.db.DBConnectionPool;
+import com.gojek.esb.sink.db.DBSink;
+import com.gojek.esb.sink.db.HikariDBConnectionPool;
 import com.gojek.esb.util.TimeUtil;
 import org.aeonbits.owner.ConfigFactory;
 import org.asynchttpclient.DefaultAsyncHttpClient;
@@ -51,12 +59,37 @@ public class LogConsumerFactory {
 
         Sink sink;
         if (appConfig.getSinkType() == SinkType.DB) {
-            DBConfig dbConfig = ConfigFactory.create(DBConfig.class, config);
-            sink = new DBSink(dbConfig);
-
+            sink = createDBSink();
         } else {
             sink = new HttpSink(FactoryUtils.httpClient);
         }
         return new LogConsumer(consumer, sink, FactoryUtils.statsDClient, FactoryUtils.clockInstance);
+    }
+
+    private Sink createDBSink() {
+        DBConfig dbConfig = ConfigFactory.create(DBConfig.class, config);
+        DBBatchCommand dbBatchCommand = createBatchCommand(dbConfig);
+        QueryTemplate queryTemplate = createQueryTemplate(dbConfig);
+
+        return new DBSink(dbBatchCommand, queryTemplate);
+    }
+
+    private DBBatchCommand createBatchCommand(DBConfig dbConfig) {
+        long connectionTimeout = dbConfig.getConnectionTimeout();
+        long idleTimeout = dbConfig.getIdleTimeout();
+        DBConnectionPool connectionPool = new HikariDBConnectionPool(dbConfig.getDbUrl(), dbConfig.getUser(),
+                dbConfig.getPassword(), dbConfig.getMaximumConnectionPoolSize(),
+                connectionTimeout, idleTimeout);
+
+        BackOffProvider backOffProvider = new ExponentialBackOffProvider(dbConfig.getInitialExpiryTimeInMs(),
+                dbConfig.getBackOffRate(), dbConfig.getMaximumExpiryInMs());
+        RetrySinkCommand retrySinkCommand = new RetrySinkCommand(backOffProvider);
+
+        return new DBBatchCommand(connectionPool, retrySinkCommand);
+    }
+
+    private QueryTemplate createQueryTemplate(DBConfig dbConfig) {
+        ProtoToTableMapper protoToTableMapper = new ProtoToTableMapper(dbConfig.getProtoSchema());
+        return new QueryTemplate(dbConfig, protoToTableMapper);
     }
 }
