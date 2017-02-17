5bb67b2721604d975f7c1f9aa627a5c036221cb0
Ashwin Sinha <aswinsina@gmail.com>
2017-02-17 13:00:05 +0530

    Adding some configs to make integration tests work

--- src/integration-test/java/com/gojek/esb/LogConsumerFactoryTest.java
+++ src/integration-test/java/com/gojek/esb/LogConsumerFactoryTest.java
@@ -21,7 +21,7 @@ public class LogConsumerFactoryTest {
         config = new HashMap<String, String>() {{
             put("KAFKA_TOPIC", "some topic");
             put("KAFKA_ADDRESS", "localhost:1111");
-
+            put("LOGSINK_PROTO_SCHEMA", "com.gojek.esb.driverlocation.DriverLocationLogMessage");
         }};
     }
 
@@ -31,13 +31,13 @@ public class LogConsumerFactoryTest {
         LogConsumerFactory logConsumerFactory = new LogConsumerFactory(config);
 
         Assert.assertEquals(logConsumerFactory.getConsumer().getClass(), LogConsumer.class);
-
     }
 
     @Test
     public void shouldCreateHttpSinkConsumer() {
         config.put("STREAMING", "FALSE");
-        config.put("SINK", "HTTP");
+        config.put("SINK", "http");
+        config.put("HTTPSINK_RETRY_STATUS_CODE_RANGES", "405-500,501-600");
         LogConsumerFactory logConsumerFactory = new LogConsumerFactory(config);
 
         Assert.assertEquals(logConsumerFactory.getConsumer().getSink().getClass(), HttpSink.class);
@@ -58,9 +58,9 @@ public class LogConsumerFactoryTest {
             put("PROTO_SCHEMA", "com.gojek.esb.feedback.FeedbackLogMessage");
             put("JDBC_DRIVER", "org.postgresql.Driver");
             put("TABLE_NAME", "dfs.feedback_message");
-            put("DB_USERNAME", "deepakmarathe");
-            put("DB_PASSWORD", "deepakmarathe");
-            put("DB_URL", "jdbc:postgresql://127.0.0.1:5432/deepakmarathe");
+            put("DB_USERNAME", "postgres");
+            put("DB_PASSWORD", "");
+            put("DB_URL", "jdbc:postgresql://127.0.0.1:5432/");
 
             put("PROTO_TO_COLUMN_MAPPING", "{\"1\":\"order_number\",\"2\":\"event_timestamp\",\"3\":\"driver_id\",\"4\":\"customer_id\",\"5\":\"feedback_rating\",\"6\":\"feedback_comment\"}");
             put("UNIQUE_KEYS", "order_number, event_timestamp");
@@ -68,8 +68,9 @@ public class LogConsumerFactoryTest {
             put("BACKOFF_RATE", "2");
             put("MAXIMUM_EXPIRY_TIME_IN_MS", "60000");
             put("MAXIMUM_CONNECTION_POOL_SIZE", "10");
-            put("DB_CONNECTION_TIMEOUT", "250");
-            put("DB_IDLE_TIMEOUT", "10");
+            put("DB_CONNECTION_TIMEOUT_MS", "250");
+            put("DB_IDLE_TIMEOUT_MS", "30000");
+            put("DB_MINIMUM_IDLE", "0");
         }};
     }
 }
