97502cd59572ffc9450669075ed4e7a3463a46d2
Sidu Ponnappa <ckponnappa@gmail.com>
2017-02-28 02:09:55 +0530

    Updated logger, changed task runConsumer to also use externally set env vars over those loaded from build .properties, updated README

--- src/main/java/com/gojek/esb/launch/Main.java
+++ src/main/java/com/gojek/esb/launch/Main.java
@@ -7,8 +7,7 @@ import java.io.IOException;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-
-        LogConsumer logConsumer = new LogConsumerFactory(System.getenv()).getConsumer();
+        LogConsumer logConsumer = new LogConsumerFactory(System.getenv()).buildConsumer();
         //TODO:move initialize call to log consumer
         logConsumer.getSink().initialize();
         while (true) {
