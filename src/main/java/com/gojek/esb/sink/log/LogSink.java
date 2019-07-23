29d54c08044b67d6d7eb7909dbd7b609bdf028a4
rohilsurana <rohilsurana96@gmail.com>
2019-07-23 17:00:33 +0530

    [Rohil] Log sink to log only key or message on the basis of

--- src/main/java/com/gojek/esb/sink/log/LogSink.java
+++ src/main/java/com/gojek/esb/sink/log/LogSink.java
@@ -1,11 +1,8 @@
 package com.gojek.esb.sink.log;
 
-import com.gojek.de.stencil.parser.ProtoParser;
 import com.gojek.esb.consumer.EsbMessage;
 import com.gojek.esb.sink.Sink;
-import com.google.protobuf.DynamicMessage;
-import com.google.protobuf.InvalidProtocolBufferException;
-import org.slf4j.LoggerFactory;
+import lombok.AllArgsConstructor;
 
 import java.io.IOException;
 import java.util.ArrayList;
@@ -15,42 +12,18 @@ import java.util.List;
  * Sink implementation to write the messages read from kafka to the console.
  * The related configurations for LogSink can be found here: {@see com.gojek.esb.config.LogConfig}
  */
+@AllArgsConstructor
 public class LogSink implements Sink {
 
-    private ProtoParser protoParser;
+    private KeyOrMessageParser parser;
     private ProtoLogger protoLogger;
-    private static final org.slf4j.Logger LOGGER = LoggerFactory.getLogger(LogSink.class);
-
-    public LogSink(ProtoParser protoParser, ProtoLogger protoLogger) {
-        this.protoParser = protoParser;
-        this.protoLogger = protoLogger;
-    }
 
     @Override
     public List<EsbMessage> pushMessage(List<EsbMessage> esbMessages) throws IOException {
         for (EsbMessage message : esbMessages) {
-            DynamicMessage key = null;
-            byte[] logKey = message.getLogKey();
-            if (logKey != null) {
-                try {
-                    key = parse(logKey);
-                } catch (IOException e) {
-                    LOGGER.warn("Unable to parse logKey", e);
-                }
-            }
-            protoLogger.log(key, parse(message.getLogMessage()));
+            protoLogger.log(parser.parse(message));
         }
-        return new ArrayList<EsbMessage>();
-    }
-
-    private DynamicMessage parse(byte[] data) throws IOException {
-        DynamicMessage dynamicMessage = null;
-        try {
-            dynamicMessage = protoParser.parse(data);
-        } catch (InvalidProtocolBufferException e) {
-            throw new IOException(e);
-        }
-        return dynamicMessage;
+        return new ArrayList<>();
     }
 
     @Override
