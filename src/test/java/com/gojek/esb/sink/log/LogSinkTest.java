939c0fa3bcf161f388c26a1031f639e36fa80b8a
rohilsurana <rohilsurana96@gmail.com>
2019-07-23 17:00:33 +0530

    [Rohil] Log sink to log only key or message on the basis of

--- src/test/java/com/gojek/esb/sink/log/LogSinkTest.java
+++ src/test/java/com/gojek/esb/sink/log/LogSinkTest.java
@@ -1,13 +1,11 @@
 package com.gojek.esb.sink.log;
 
-import com.gojek.de.stencil.parser.ProtoParser;
 import com.gojek.esb.consumer.EsbMessage;
 import com.gojek.esb.consumer.TestKey;
 import com.gojek.esb.consumer.TestMessage;
 import com.gojek.esb.exception.DeserializerException;
 import com.gojek.esb.sink.Sink;
 import com.google.protobuf.DynamicMessage;
-import com.google.protobuf.InvalidProtocolBufferException;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -27,18 +25,18 @@ public class LogSinkTest {
     private ProtoLogger protoLogger;
 
     @Mock
-    private ProtoParser protoParser;
+    private KeyOrMessageParser parser;
 
     private DynamicMessage dynamicMessage = DynamicMessage.newBuilder(TestMessage.getDescriptor()).build();
 
     private Sink sink;
 
     @Before
-    public void setup() throws InvalidProtocolBufferException {
-        Mockito.doNothing().when(protoLogger).log(Mockito.any(DynamicMessage.class), Mockito.any(DynamicMessage.class));
-        Mockito.when(protoParser.parse(Mockito.any(byte[].class))).thenReturn(dynamicMessage);
+    public void setup() throws IOException {
+        Mockito.doNothing().when(protoLogger).log(Mockito.any(DynamicMessage.class));
+        Mockito.when(parser.parse(Mockito.any(EsbMessage.class))).thenReturn(dynamicMessage);
 
-        sink = new LogSink(protoParser, protoLogger);
+        sink = new LogSink(parser, protoLogger);
     }
 
     @Test
@@ -47,16 +45,17 @@ public class LogSinkTest {
 
         sink.pushMessage(esbMessages);
 
-        Mockito.verify(protoLogger, Mockito.times(1)).log(Mockito.any(DynamicMessage.class), Mockito.any(DynamicMessage.class));
+        Mockito.verify(protoLogger, Mockito.times(1)).log(Mockito.any(DynamicMessage.class));
     }
 
     @Test
     public void shouldParseProto() throws IOException, InvocationTargetException, IllegalAccessException, DeserializerException {
-        List<EsbMessage> esbMessages = Arrays.asList(new EsbMessage(new byte[0], new byte[0], "topic", 0, 100));
+        List<EsbMessage> esbMessages = Arrays.asList(new EsbMessage(new byte[0], new byte[0], "topic", 0, 100),
+                new EsbMessage(new byte[0], new byte[0], "topic-2", 0, 100));
 
         sink.pushMessage(esbMessages);
 
-        Mockito.verify(protoParser, Mockito.times(2)).parse(Mockito.any(byte[].class));
+        Mockito.verify(parser, Mockito.times(2)).parse(Mockito.any(EsbMessage.class));
     }
 
 
@@ -68,7 +67,7 @@ public class LogSinkTest {
 
         sink.pushMessage(esbMessages);
 
-        Mockito.verify(protoLogger, Mockito.times(1)).log(Mockito.any(DynamicMessage.class), Mockito.any(DynamicMessage.class));
+        Mockito.verify(protoLogger, Mockito.times(1)).log(Mockito.any(DynamicMessage.class));
     }
 
     @Test
@@ -77,7 +76,7 @@ public class LogSinkTest {
 
         sink.pushMessage(Arrays.asList(new EsbMessage(null, testMessage, "topic", 0, 100)));
 
-        Mockito.verify(protoLogger, Mockito.times(1)).log(Mockito.isNull(), Mockito.any(DynamicMessage.class));
-        Mockito.verify(protoParser, Mockito.never()).parse(null);
+        Mockito.verify(protoLogger, Mockito.times(1)).log(Mockito.any(DynamicMessage.class));
+        Mockito.verify(parser, Mockito.never()).parse(null);
     }
 }
