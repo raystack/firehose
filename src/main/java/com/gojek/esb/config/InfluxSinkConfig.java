2b9f8bf1f0cc1883b1a646a4035e011b07104530
Rohil Surana <rohil.s@go-jek.com>
2019-08-24 00:04:30 +0530

    [Rohil] Support nested messages in InfluxDB sink

--- src/main/java/com/gojek/esb/config/InfluxSinkConfig.java
+++ src/main/java/com/gojek/esb/config/InfluxSinkConfig.java
@@ -1,16 +1,17 @@
 package com.gojek.esb.config;
 
-import com.gojek.esb.config.converter.FieldNameProtoIndexConverter;
+import com.gojek.esb.config.converter.ProtoIndexToFieldMapConverter;
 import org.aeonbits.owner.Config;
+
 import java.util.Properties;
 
 public interface InfluxSinkConfig extends DBSinkConfig {
     @Config.Key("FIELD_NAME_PROTO_INDEX_MAPPING")
-    @Config.ConverterClass(FieldNameProtoIndexConverter.class)
+    @Config.ConverterClass(ProtoIndexToFieldMapConverter.class)
     Properties getFieldNameProtoIndexMapping();
 
     @Config.Key("TAG_NAME_PROTO_INDEX_MAPPING")
-    @Config.ConverterClass(FieldNameProtoIndexConverter.class)
+    @Config.ConverterClass(ProtoIndexToFieldMapConverter.class)
     Properties getTagNameProtoIndexMapping();
 
     @Config.Key("MEASUREMENT_NAME")
