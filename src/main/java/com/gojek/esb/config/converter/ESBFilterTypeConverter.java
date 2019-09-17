f1c0baed854bfa0cd560af91155b0f36522ffc5a
Nikhil Jhaveri <nikhil.jhaveri@go-jek.com>
2019-09-17 14:35:31 +0530

    [Nikhil] Fixing issue on empty filter type in ESB filter type init.

--- src/main/java/com/gojek/esb/config/converter/ESBFilterTypeConverter.java
+++ src/main/java/com/gojek/esb/config/converter/ESBFilterTypeConverter.java
@@ -2,12 +2,17 @@ package com.gojek.esb.config.converter;
 
 import com.gojek.esb.config.enums.EsbFilterType;
 import org.aeonbits.owner.Converter;
+import org.apache.commons.lang3.StringUtils;
 
 import java.lang.reflect.Method;
 
 public class ESBFilterTypeConverter implements Converter<EsbFilterType> {
     @Override
     public EsbFilterType convert(Method method, String input) {
-        return EsbFilterType.valueOf(input.toUpperCase());
+        if (StringUtils.isNotEmpty(input)) {
+            return EsbFilterType.valueOf(input.toUpperCase());
+        } else {
+            return EsbFilterType.NONE;
+        }
     }
 }
