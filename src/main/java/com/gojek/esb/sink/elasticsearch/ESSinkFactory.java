072f48787c6a4b790243ec48c71156b3d0d61c1f
MayurGubrele <mayur.gubrele@gmail.com>
2020-02-25 16:58:50 +0530

    | [Mayur | Prakhar] | Add sync ES client instead of async

--- src/main/java/com/gojek/esb/sink/elasticsearch/ESSinkFactory.java
+++ src/main/java/com/gojek/esb/sink/elasticsearch/ESSinkFactory.java
@@ -3,34 +3,52 @@ package com.gojek.esb.sink.elasticsearch;
 import com.gojek.de.stencil.client.StencilClient;
 import com.gojek.de.stencil.parser.ProtoParser;
 import com.gojek.esb.config.ESSinkConfig;
-import com.gojek.esb.config.enums.ESRequestType;
+import com.gojek.esb.config.enums.SinkType;
+import com.gojek.esb.metrics.Instrumentation;
 import com.gojek.esb.metrics.StatsDReporter;
 import com.gojek.esb.serializer.EsbMessageToJson;
 import com.gojek.esb.sink.Sink;
 import com.gojek.esb.sink.SinkFactory;
-import com.gojek.esb.sink.elasticsearch.client.ESSinkClient;
+import com.gojek.esb.sink.elasticsearch.request.ESRequestHandler;
+import com.gojek.esb.sink.elasticsearch.request.ESRequestHandlerFactory;
 import org.aeonbits.owner.ConfigFactory;
+import org.apache.http.HttpHost;
+import org.elasticsearch.client.RestClient;
+import org.elasticsearch.client.RestHighLevelClient;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.util.Map;
 
-import static com.gojek.esb.config.enums.ESRequestType.INSERT_OR_UPDATE;
-import static com.gojek.esb.config.enums.ESRequestType.UPDATE_ONLY;
-
-
 public class ESSinkFactory implements SinkFactory {
+    private static final Logger LOGGER = LoggerFactory.getLogger(ESSinkFactory.class.getName());
 
     @Override
-    public Sink create(Map<String, String> configuration, StatsDReporter client, StencilClient stencilClient) {
+    public Sink create(Map<String, String> configuration, StatsDReporter statsDReporter, StencilClient stencilClient) {
         ESSinkConfig esSinkConfig = ConfigFactory.create(ESSinkConfig.class, configuration);
-        ESRequestType esRequestType = esSinkConfig.isUpdateOnlyMode() ? UPDATE_ONLY : INSERT_OR_UPDATE;
-        ESRequestBuilder esRequestBuilder = new ESRequestBuilder(
-                esRequestType,
-                esSinkConfig.getEsIdFieldName(),
-                esSinkConfig.getESMessageType(),
-                new EsbMessageToJson(
-                        new ProtoParser(stencilClient, esSinkConfig.getProtoSchema()), esSinkConfig.shouldPreserveProtoFieldNames())
-        );
-        ESSinkClient esSinkClient = new ESSinkClient(esSinkConfig, client);
-        return new ESSink(esRequestBuilder, esSinkClient, esSinkConfig.getEsTypeName(), esSinkConfig.getEsIndexName());
+        ESRequestHandler esRequestHandler = new ESRequestHandlerFactory(esSinkConfig, esSinkConfig.getEsIdFieldName(), esSinkConfig.getESMessageType(), new EsbMessageToJson(
+                new ProtoParser(stencilClient, esSinkConfig.getProtoSchema()), esSinkConfig.shouldPreserveProtoFieldNames()),
+                esSinkConfig.getEsTypeName(),
+                esSinkConfig.getEsIndexName())
+                .getRequestHandler();
+
+        HttpHost[] httpHosts = getHttpHosts(esSinkConfig.getEsConnectionUrls());
+        RestHighLevelClient client = new RestHighLevelClient(RestClient.builder(httpHosts));
+        return new ESSink(new Instrumentation(statsDReporter, ESSink.class), SinkType.ELASTICSEARCH.name(), client, esRequestHandler);
+    }
+
+    HttpHost[] getHttpHosts(String esConnectionUrls) {
+        if (esConnectionUrls != null && !esConnectionUrls.isEmpty()) {
+            String[] esNodes = esConnectionUrls.trim().split(",");
+            HttpHost[] httpHosts = new HttpHost[esNodes.length];
+            for (int i = 0; i < esNodes.length; i++) {
+                String[] node = esNodes[i].trim().split(":");
+                httpHosts[i] = new HttpHost(node[0].trim(), Integer.parseInt(node[1].trim()));
+            }
+            return httpHosts;
+        } else {
+            LOGGER.error("ES_CONNECTION_URLS is empty or null");
+            throw new IllegalArgumentException("ES_CONNECTION_URLS is empty or null");
+        }
     }
 }
--- src/test/java/com/gojek/esb/sink/elasticsearch/ESSinkFactoryTest.java
+++ src/test/java/com/gojek/esb/sink/elasticsearch/ESSinkFactoryTest.java
@@ -3,22 +3,17 @@ package com.gojek.esb.sink.elasticsearch;
 import com.gojek.de.stencil.client.StencilClient;
 import com.gojek.esb.metrics.StatsDReporter;
 import com.gojek.esb.sink.Sink;
-import org.junit.After;
+import org.apache.http.HttpHost;
 import org.junit.Before;
 import org.junit.Test;
-import org.junit.runner.RunWith;
 import org.mockito.Mock;
-import org.mockito.runners.MockitoJUnitRunner;
 
 import java.util.HashMap;
 import java.util.Map;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
-@RunWith(MockitoJUnitRunner.class)
 public class ESSinkFactoryTest {
-
-    private ESSinkFactory esSinkFactory;
 
     private Map<String, String> configuration;
 
@@ -29,38 +24,83 @@ public class ESSinkFactoryTest {
     private StencilClient stencilClient;
 
     @Before
-    public void setUp() throws Exception {
-        esSinkFactory = new ESSinkFactory();
+    public void setUp() {
         configuration = new HashMap<>();
-    }
-
-    @After
-    public void tearDown() throws Exception {
-        esSinkFactory = null;
     }
 
     @Test
     public void shouldCreateESSink() {
-        configuration.put("ES_BATCH_RETRY_COUNT", "3");
-        configuration.put("ES_BATCH_SIZE", "1000");
-        configuration.put("ES_CONNECTION_URLS", "localhost: 9200 , localhost : 9200 ");
+        configuration.put("ES_CONNECTION_URLS", "localhost:9200 , localhost:9200 ");
+        ESSinkFactory esSinkFactory = new ESSinkFactory();
         Sink sink = esSinkFactory.create(configuration, statsDReporter, stencilClient);
-        assertTrue(sink instanceof ESSink);
+        assertEquals(ESSink.class, sink.getClass());
     }
 
     @Test
-    public void shouldCreateUpdateOnlyESSink() {
-        configuration.put("ES_BATCH_RETRY_COUNT", "3");
-        configuration.put("ES_BATCH_SIZE", "1000");
-        configuration.put("ES_CONNECTION_URLS", "localhost: 9200 , localhost : 9200 ");
-        configuration.put("ES_UPDATE_ONLY_MODE", "true");
-        Sink sink = esSinkFactory.create(configuration, statsDReporter, stencilClient);
-        assertTrue(sink instanceof ESSink);
+    public void shouldThrowIllegalArgumentExceptionForEmptyESConnectionURLs() {
+        ESSinkFactory esSinkFactory = new ESSinkFactory();
+        try {
+            esSinkFactory.getHttpHosts("");
+        } catch (Exception e) {
+            assertEquals(IllegalArgumentException.class, e.getClass());
+            assertEquals("ES_CONNECTION_URLS is empty or null", e.getMessage());
+        }
     }
 
-    @Test(expected = IllegalArgumentException.class)
-    public void shouldNotBeAbleToCreateESSink() {
-        esSinkFactory.create(configuration, statsDReporter, stencilClient);
-        fail("Should not have reached here");
+    @Test
+    public void shouldThrowIllegalArgumentExceptionForNullESConnectionURLs() {
+        ESSinkFactory esSinkFactory = new ESSinkFactory();
+        try {
+            esSinkFactory.getHttpHosts(null);
+        } catch (Exception e) {
+            assertEquals(IllegalArgumentException.class, e.getClass());
+            assertEquals("ES_CONNECTION_URLS is empty or null", e.getMessage());
+        }
     }
+
+    @Test
+    public void shouldThrowIllegalArgumentExceptionForEmptyHostName() {
+        ESSinkFactory esSinkFactory = new ESSinkFactory();
+        String esConnectionURLs = ":1000";
+        try {
+            esSinkFactory.getHttpHosts(esConnectionURLs);
+        } catch (Exception e) {
+            assertEquals(IllegalArgumentException.class, e.getClass());
+        }
+    }
+
+    @Test
+    public void shouldGetHttpHostsForValidESConnectionURLs() {
+        ESSinkFactory esSinkFactory = new ESSinkFactory();
+        String esConnectionURLs = "localhost_1:1000,localhost_2:1000";
+        HttpHost[] httpHosts = esSinkFactory.getHttpHosts(esConnectionURLs);
+
+        assertEquals("localhost_1", httpHosts[0].getHostName());
+        assertEquals(1000, httpHosts[0].getPort());
+        assertEquals("localhost_2", httpHosts[1].getHostName());
+        assertEquals(1000, httpHosts[1].getPort());
+    }
+
+    @Test
+    public void shouldGetHttpHostsForValidESConnectionURLsWithSpacesInBetween() {
+        ESSinkFactory esSinkFactory = new ESSinkFactory();
+        String esConnectionURLs = " localhost_1: 1000,  localhost_2:1000";
+        HttpHost[] httpHosts = esSinkFactory.getHttpHosts(esConnectionURLs);
+
+        assertEquals("localhost_1", httpHosts[0].getHostName());
+        assertEquals(1000, httpHosts[0].getPort());
+        assertEquals("localhost_2", httpHosts[1].getHostName());
+        assertEquals(1000, httpHosts[1].getPort());
+    }
+
+    @Test
+    public void shouldGetHttpHostsForIPInESConnectionURLs() {
+        ESSinkFactory esSinkFactory = new ESSinkFactory();
+        String esConnectionURLs = "172.28.32.156:1000";
+        HttpHost[] httpHosts = esSinkFactory.getHttpHosts(esConnectionURLs);
+
+        assertEquals("172.28.32.156", httpHosts[0].getHostName());
+        assertEquals(1000, httpHosts[0].getPort());
+    }
+
 }
