a84494771a411f3885874a78c1f86232de8efac5
MayurGubrele <mayur.gubrele@gmail.com>
2020-02-27 10:31:28 +0530

    | [Mayur | Prakhar] | Add tests for es insert, update request handler and refactor request handler

--- src/main/java/com/gojek/esb/sink/elasticsearch/ESSink.java
+++ src/main/java/com/gojek/esb/sink/elasticsearch/ESSink.java
@@ -8,6 +8,7 @@ import com.gojek.esb.sink.elasticsearch.request.ESRequestHandler;
 import org.elasticsearch.action.bulk.BulkRequest;
 import org.elasticsearch.action.bulk.BulkResponse;
 import org.elasticsearch.client.RestHighLevelClient;
+import org.elasticsearch.common.unit.TimeValue;
 
 import java.io.IOException;
 import java.util.ArrayList;
@@ -17,16 +18,19 @@ public class ESSink extends AbstractSink {
     private RestHighLevelClient client;
     private ESRequestHandler esRequestHandler;
     private BulkRequest bulkRequest;
+    private long esRequestTimeoutInMs;
 
-    public ESSink(Instrumentation instrumentation, String sinkType, RestHighLevelClient client, ESRequestHandler esRequestHandler) {
+    public ESSink(Instrumentation instrumentation, String sinkType, RestHighLevelClient client, ESRequestHandler esRequestHandler, long esRequestTimeoutInMs) {
         super(instrumentation, sinkType);
         this.client = client;
         this.esRequestHandler = esRequestHandler;
+        this.esRequestTimeoutInMs = esRequestTimeoutInMs;
     }
 
     @Override
     protected void prepare(List<EsbMessage> esbMessages) {
         bulkRequest = new BulkRequest();
+        bulkRequest.timeout(TimeValue.timeValueMillis(esRequestTimeoutInMs));
         esbMessages.forEach(esbMessage -> bulkRequest.add(esRequestHandler.getRequest(esbMessage)));
     }
 
