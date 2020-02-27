5a6d380a01a6fc7c24d5ede0ec2b00f6b0460eb2
MayurGubrele <mayur.gubrele@gmail.com>
2020-02-27 10:31:28 +0530

    | [Mayur | Prakhar] | Add tests for es insert, update request handler and refactor request handler

--- src/main/java/com/gojek/esb/sink/elasticsearch/request/ESRequestHandler.java
+++ src/main/java/com/gojek/esb/sink/elasticsearch/request/ESRequestHandler.java
@@ -1,74 +1,53 @@
 package com.gojek.esb.sink.elasticsearch.request;
 
 import com.gojek.esb.config.enums.ESMessageType;
-import com.gojek.esb.config.enums.ESRequestType;
 import com.gojek.esb.consumer.EsbMessage;
-import com.gojek.esb.exception.DeserializerException;
+import com.gojek.esb.exception.JsonParseException;
 import com.gojek.esb.serializer.EsbMessageToJson;
-import lombok.Getter;
 import org.elasticsearch.action.DocWriteRequest;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
 
 import java.nio.charset.Charset;
-import java.util.Optional;
 
 import static com.gojek.esb.config.enums.ESMessageType.PROTOBUF;
 
-@Getter
 public abstract class ESRequestHandler {
-    private final ESRequestType esRequestType;
-    private final String esIdFieldName;
     private final ESMessageType messageType;
     private final EsbMessageToJson jsonSerializer;
-    private final String esTypeName;
-    private final String esIndexName;
     private final JSONParser jsonParser;
 
-    public ESRequestHandler(ESRequestType esRequestType, String esIdFieldName, ESMessageType messageType, EsbMessageToJson jsonSerializer, String esTypeName, String esIndexName) {
-        this.esRequestType = esRequestType;
-        this.esIdFieldName = esIdFieldName;
+    public ESRequestHandler(ESMessageType messageType, EsbMessageToJson jsonSerializer) {
         this.messageType = messageType;
         this.jsonSerializer = jsonSerializer;
-        this.esTypeName = esTypeName;
-        this.esIndexName = esIndexName;
         this.jsonParser = new JSONParser();
     }
 
-    public abstract boolean canCreate(ESRequestType requestType);
+    public abstract boolean canCreate();
 
     public abstract DocWriteRequest getRequest(EsbMessage esbMessage);
 
-    protected String extractId(EsbMessage message) {
-        String payload = extractPayload(message);
-        return getStringFromJson(payload, esIdFieldName);
-    }
-
-    protected String extractPayload(EsbMessage message) {
+    String extractPayload(EsbMessage message) {
         if (messageType.equals(PROTOBUF)) {
-            return extractPayloadFromProtobuf(message);
+            return getFieldFromJSON(jsonSerializer.serialize(message), "logMessage");
         }
         return new String(message.getLogMessage(), Charset.defaultCharset());
     }
 
-    private String extractPayloadFromProtobuf(EsbMessage message) {
+    String getFieldFromJSON(String jsonString, String key) {
         try {
-            return getStringFromJson(jsonSerializer.serialize(message), "logMessage");
-        } catch (DeserializerException e) {
-            e.printStackTrace();
-            throw new RuntimeException(e.getMessage(), e.getCause());
-        }
-    }
-
-    private String getStringFromJson(String payload, String fieldName) {
-        try {
-            JSONObject parse = (JSONObject) jsonParser.parse(payload);
-            return Optional.of(parse.get(fieldName)).get().toString();
+            JSONObject parse = (JSONObject) jsonParser.parse(jsonString);
+            Object valueAtKey = parse.get(key);
+            if (valueAtKey == null) {
+                throw new IllegalArgumentException("Key: " + key + " not found in ESB Message");
+            }
+            return valueAtKey.toString();
         } catch (ParseException e) {
-            throw new RuntimeException(e.getMessage(), e.getCause());
+            throw new JsonParseException(e.getMessage(), e.getCause());
         } finally {
             jsonParser.reset();
         }
     }
+
 }
