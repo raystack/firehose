50cc6fb97484eedeab7c4dde68de9697f5d0235c
MayurGubrele <mayur.gubrele@gmail.com>
2020-03-03 15:05:24 +0530

    [Prakhar | Mayur] fix to shut the firehose down in case of IOException

--- src/main/java/com/gojek/esb/launch/Main.java
+++ src/main/java/com/gojek/esb/launch/Main.java
@@ -3,15 +3,11 @@ package com.gojek.esb.launch;
 import com.gojek.esb.config.KafkaConsumerConfig;
 import com.gojek.esb.consumer.FireHoseConsumer;
 import com.gojek.esb.consumer.Task;
-import com.gojek.esb.exception.DeserializerException;
 import com.gojek.esb.factory.FireHoseConsumerFactory;
-import com.gojek.esb.filter.EsbFilterException;
 import com.gojek.esb.metrics.Instrumentation;
 import com.gojek.esb.metrics.StatsDReporter;
 import com.gojek.esb.metrics.StatsDReporterFactory;
-
 import org.aeonbits.owner.ConfigFactory;
-import org.apache.kafka.clients.consumer.CommitFailedException;
 
 import java.io.IOException;
 
@@ -24,37 +20,34 @@ public class Main {
 
     private static void multiThreadedConsumers(KafkaConsumerConfig kafkaConsumerConfig) throws InterruptedException {
         StatsDReporter statsDReporter = StatsDReporterFactory
-        .fromKafkaConsumerConfig(kafkaConsumerConfig)
-        .buildReporter();
+                .fromKafkaConsumerConfig(kafkaConsumerConfig)
+                .buildReporter();
         Instrumentation instrumentation = new Instrumentation(statsDReporter, Main.class);
         Task consumerTask = new Task(
-            kafkaConsumerConfig.noOfConsumerThreads(),
-            kafkaConsumerConfig.threadCleanupDelay(),
-            new Instrumentation(statsDReporter, Task.class),
-            taskFinished -> {
+                kafkaConsumerConfig.noOfConsumerThreads(),
+                kafkaConsumerConfig.threadCleanupDelay(),
+                new Instrumentation(statsDReporter, Task.class),
+                taskFinished -> {
 
-            FireHoseConsumer fireHoseConsumer = null;
-            try {
-                fireHoseConsumer = new FireHoseConsumerFactory(kafkaConsumerConfig).buildConsumer();
-                while (true) {
+                    FireHoseConsumer fireHoseConsumer = null;
                     try {
-                        if (Thread.interrupted()) {
-                            instrumentation.logInfo("Consumer Thread interrupted, leaving the loop!");
-                            break;
+                        fireHoseConsumer = new FireHoseConsumerFactory(kafkaConsumerConfig).buildConsumer();
+                        while (true) {
+                            if (Thread.interrupted()) {
+                                instrumentation.logInfo("Consumer Thread interrupted, leaving the loop!");
+                                break;
+                            }
+                            fireHoseConsumer.processPartitions();
+
                         }
-                        fireHoseConsumer.processPartitions();
-                    } catch (IOException | DeserializerException | EsbFilterException | CommitFailedException e) {
-                        instrumentation.captureNonFatalError(e, "Exception in Consumer Thread {} {} continuing", e.getMessage(), e);
+                    } catch (Exception e) {
+                        instrumentation.captureFatalError(e, "Exception on creating the consumer, exiting the application");
+                        System.exit(1);
+                    } finally {
+                        ensureThreadInterruptStateIsClearedAndClose(fireHoseConsumer, instrumentation);
+                        taskFinished.run();
                     }
-                }
-            } catch (Exception e) {
-                instrumentation.captureFatalError(e, "Exception on creating the consumer, exiting the application");
-                System.exit(1);
-            } finally {
-                ensureThreadInterruptStateIsClearedAndClose(fireHoseConsumer, instrumentation);
-                taskFinished.run();
-            }
-        });
+                });
 
 
         Runtime.getRuntime().addShutdownHook(new Thread(() -> {
