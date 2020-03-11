8103b984f5f4f1ebb0ee5c659347efa949449eed
arujit <jamesbondukela@gmail.com>
2020-03-11 10:14:32 +0530

    [arujit | Rasyid]  add invalidConfigurationException for invalid TTL

--- src/test/java/com/gojek/esb/sink/redis/ttl/RedisTTLFactoryTest.java
+++ src/test/java/com/gojek/esb/sink/redis/ttl/RedisTTLFactoryTest.java
@@ -2,9 +2,12 @@ package com.gojek.esb.sink.redis.ttl;
 
 import com.gojek.esb.config.RedisSinkConfig;
 import com.gojek.esb.config.enums.RedisTTLType;
+import com.gojek.esb.exception.EglcConfigurationException;
 import org.junit.Assert;
 import org.junit.Before;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.ExpectedException;
 import org.mockito.Mock;
 
 import static org.mockito.Mockito.when;
@@ -14,6 +17,9 @@ public class RedisTTLFactoryTest {
 
     @Mock
     private RedisSinkConfig redisSinkConfig;
+
+    @Rule
+    public ExpectedException expectedException = ExpectedException.none();
 
     @Before
     public void setup() {
@@ -41,5 +47,15 @@ public class RedisTTLFactoryTest {
         when(redisSinkConfig.getRedisTTLValue()).thenReturn(100L);
         RedisTTL redisTTL = RedisTTLFactory.getTTl(redisSinkConfig);
         Assert.assertEquals(redisTTL.getClass(), DurationTTL.class);
+    }
+
+    @Test
+    public void shouldThrowExceptionInCaseOfInvalidConfiguration() {
+        expectedException.expect(EglcConfigurationException.class);
+        expectedException.expectMessage("Provide a positive TTL value");
+
+        when(redisSinkConfig.getRedisTTLType()).thenReturn(RedisTTLType.DURATION);
+        when(redisSinkConfig.getRedisTTLValue()).thenReturn(-1L);
+        RedisTTLFactory.getTTl(redisSinkConfig);
     }
 }
