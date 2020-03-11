6e4bb7a100384e38d61eb20683e3ae5d168bedf2
arujit <jamesbondukela@gmail.com>
2020-03-11 10:14:32 +0530

    [arujit | Rasyid]  add invalidConfigurationException for invalid TTL

--- src/main/java/com/gojek/esb/sink/redis/ttl/RedisTTLFactory.java
+++ src/main/java/com/gojek/esb/sink/redis/ttl/RedisTTLFactory.java
@@ -1,15 +1,20 @@
 package com.gojek.esb.sink.redis.ttl;
 
 import com.gojek.esb.config.RedisSinkConfig;
+import com.gojek.esb.exception.EglcConfigurationException;
 
 public class RedisTTLFactory {
 
     public static RedisTTL getTTl(RedisSinkConfig redisSinkConfig) {
+        long redisTTLValue = redisSinkConfig.getRedisTTLValue();
+        if (redisTTLValue < 0) {
+            throw new EglcConfigurationException("Provide a positive TTL value");
+        }
         switch (redisSinkConfig.getRedisTTLType()) {
             case EXACT_TIME:
-                return new ExactTimeTTL(redisSinkConfig.getRedisTTLValue());
+                return new ExactTimeTTL(redisTTLValue);
             case DURATION:
-                return new DurationTTL((int) redisSinkConfig.getRedisTTLValue());
+                return new DurationTTL((int) redisTTLValue);
             default:
                 return new NoRedisTTL();
         }
