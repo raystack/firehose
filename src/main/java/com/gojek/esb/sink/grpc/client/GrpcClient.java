11cb00e5d1f44172e65debf6e76695f9bb6d1a0d
Ankit Tiwari <ankit@ankit.tiwari@go-jek.com>
2020-04-08 00:49:17 +0530

    initial code changes inspired from internal esb grpc sink, heavily dependent on local dependencies

---
+++ src/main/java/com/gojek/esb/sink/grpc/client/GrpcClient.java
@@ -0,0 +1,101 @@
+package com.gojek.esb.sink.grpc.client;
+
+
+import com.gojek.esb.grpc.response.GrpcResponse;
+import com.gopay.grpc.ChannelPool;
+import com.gojek.esb.config.GrpcConfig;
+import com.newrelic.api.agent.Trace;
+
+import io.grpc.Channel;
+import io.grpc.ManagedChannel;
+import io.grpc.Metadata;
+import io.grpc.MethodDescriptor;
+import io.grpc.ClientInterceptors;
+import io.grpc.CallOptions;
+import io.grpc.stub.ClientCalls;
+import io.grpc.stub.MetadataUtils;
+import org.apache.commons.io.IOUtils;
+import org.apache.kafka.common.header.Header;
+import org.apache.kafka.common.header.Headers;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+
+
+public class GrpcClient {
+
+    private ChannelPool channelPool;
+    private final GrpcConfig grpcConfig;
+    private static final Logger LOGGER = LoggerFactory.getLogger(GrpcClient.class.getName());
+
+    public GrpcClient(ChannelPool channelPool, GrpcConfig grpcConfig) {
+        this.channelPool = channelPool;
+        this.grpcConfig = grpcConfig;
+    }
+
+
+    @Trace(dispatcher = true)
+    public GrpcResponse execute(byte[] logMessage, Headers headers) {
+
+        MethodDescriptor.Marshaller<byte[]> marshaller = getMarshaller();
+        ManagedChannel managedChannel = null;
+        GrpcResponse grpcResponse;
+        try {
+            if (channelPool == null) {
+                throw new IllegalStateException("ConnectionPool was not initiated successfully");
+            }
+            managedChannel = channelPool.borrowObject(grpcConfig.getConnectionPoolMaxWaitMillis());
+
+            Metadata metadata = new Metadata();
+            for (Header header : headers) {
+                metadata.put(Metadata.Key.of(header.key(), Metadata.ASCII_STRING_MARSHALLER), new String(header.value()));
+            }
+
+            Channel decoratedChannel = ClientInterceptors.intercept(managedChannel,
+                    MetadataUtils.newAttachHeadersInterceptor(metadata));
+            byte[] response = ClientCalls.blockingUnaryCall(
+                    decoratedChannel,
+                    MethodDescriptor.create(
+                            MethodDescriptor.MethodType.UNARY,
+                            grpcConfig.getGrpcMethodUrl(),
+                            marshaller,
+                            marshaller),
+                    CallOptions.DEFAULT,
+                    logMessage);
+            grpcResponse = GrpcResponse.parseFrom(response);
+
+        } catch (Exception e) {
+            LOGGER.error("Failed to send request {} ", e.getMessage());
+            grpcResponse = GrpcResponse
+                    .newBuilder()
+                    .setSuccess(false)
+                    .build();
+        } finally {
+            if (managedChannel != null) {
+                channelPool.returnObject(managedChannel);
+            }
+        }
+        return grpcResponse;
+    }
+
+    private MethodDescriptor.Marshaller<byte[]> getMarshaller() {
+        return new MethodDescriptor.Marshaller<byte[]>() {
+            @Override
+            public InputStream stream(byte[] value) {
+                return new ByteArrayInputStream(value);
+            }
+
+            @Override
+            public byte[] parse(InputStream stream) {
+                try {
+                    return IOUtils.toByteArray(stream);
+                } catch (IOException e) {
+                    return null;
+                }
+            }
+        };
+    }
+}
