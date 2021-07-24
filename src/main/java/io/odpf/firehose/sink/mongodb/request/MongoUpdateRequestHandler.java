aefae9f5b24bb63c88be63c2cd3d7b2bcf3fc1f1
Sumit Aich <aich.1998@gmail.com>
2021-07-24 16:13:24 +0530

    feat: add MongoUpdateRequestHandler

---
+++ src/main/java/io/odpf/firehose/sink/mongodb/request/MongoUpdateRequestHandler.java
@@ -0,0 +1,35 @@
+package io.odpf.firehose.sink.mongodb.request;
+
+import com.mongodb.client.model.ReplaceOneModel;
+import io.odpf.firehose.config.enums.MongoSinkMessageType;
+import io.odpf.firehose.config.enums.MongoSinkRequestType;
+import io.odpf.firehose.consumer.Message;
+import io.odpf.firehose.serializer.MessageToJson;
+import org.bson.Document;
+
+public class MongoUpdateRequestHandler extends MongoRequestHandler {
+
+    private MongoSinkRequestType mongoSinkRequestType;
+    private String mongoPrimaryKey;
+
+    public MongoUpdateRequestHandler(MongoSinkMessageType messageType, MessageToJson jsonSerializer,  MongoSinkRequestType mongoSinkRequestType, String mongoPrimaryKey) {
+        super(messageType, jsonSerializer);
+        this.mongoSinkRequestType = mongoSinkRequestType;
+        this.mongoPrimaryKey = mongoPrimaryKey;
+    }
+
+    @Override
+    public boolean canCreate() {
+        return mongoSinkRequestType == MongoSinkRequestType.UPDATE_ONLY;
+    }
+
+    @Override
+    public ReplaceOneModel<Document> getRequest(Message message) {
+        String logMessage = extractPayload(message);
+        Document document = Document.parse(logMessage);
+
+        return new ReplaceOneModel<>(
+                new Document(mongoPrimaryKey, getFieldFromJSON(logMessage, mongoPrimaryKey)),
+                document);
+    }
+}
