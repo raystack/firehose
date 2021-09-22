b233cc1f61b0d83863326d178d9e5824280b022d
Sumit Aich <aich.1998@gmail.com>
2021-09-22 23:16:02 +0530

    test: add test for JsonFilterUtil

--- src/test/java/io/odpf/firehose/filter/json/JsonFilterUtilTest.java
+++ src/test/java/io/odpf/firehose/filter/json/JsonFilterUtilTest.java
@@ -6,7 +6,9 @@ import io.odpf.firehose.consumer.TestMessage;
 import io.odpf.firehose.metrics.Instrumentation;
 import org.aeonbits.owner.ConfigFactory;
 import org.junit.Before;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.ExpectedException;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 
@@ -17,6 +19,9 @@ import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 
 public class JsonFilterUtilTest {
+
+    @Rule
+    public ExpectedException thrown = ExpectedException.none();
 
     @Mock
     private Instrumentation instrumentation;
@@ -29,7 +34,7 @@ public class JsonFilterUtilTest {
     }
 
     @Test
-    public void shouldLogFilterTypeIfFilterTypeIsNone() {
+    public void shouldLogFilterTypeIfFilterDataSourceIsNone() {
         Map<String, String> filterConfigs = new HashMap<>();
         filterConfigs.put("FILTER_JSON_DATA_SOURCE", "none");
         filterConfig = ConfigFactory.create(FilterConfig.class, filterConfigs);
@@ -38,7 +43,7 @@ public class JsonFilterUtilTest {
     }
 
     @Test
-    public void shouldLogFilterTypeIfFilterDataSourceIsNotNoneForProtobufMessageType() {
+    public void shouldLogFilterConfigsIfFilterDataSourceIsNotNone() {
         Map<String, String> filterConfigs = new HashMap<>();
         filterConfigs.put("FILTER_JSON_DATA_SOURCE", "message");
         filterConfigs.put("FILTER_ESB_MESSAGE_FORMAT", "PROTOBUF");
@@ -49,5 +54,49 @@ public class JsonFilterUtilTest {
         verify(instrumentation, times(1)).logInfo("\n\tFilter type: {}", FilterDataSourceType.MESSAGE);
         verify(instrumentation, times(1)).logInfo("\n\tMessage Proto class: {}", TestMessage.class.getName());
         verify(instrumentation, times(1)).logInfo("\n\tFilter JSON Schema: {}", "{\"properties\":{\"order_number\":{\"const\":\"123\"}}}");
+    }
+
+    @Test
+    public void shouldThrowIllegalArgumentExceptionForNullJsonSchema() {
+        Map<String, String> filterConfigs = new HashMap<>();
+        filterConfigs.put("FILTER_JSON_DATA_SOURCE", "message");
+        filterConfigs.put("FILTER_ESB_MESSAGE_FORMAT", "PROTOBUF");
+        filterConfigs.put("FILTER_JSON_SCHEMA_PROTO_CLASS", TestMessage.class.getName());
+        filterConfig = ConfigFactory.create(FilterConfig.class, filterConfigs);
+        thrown.expect(IllegalArgumentException.class);
+        JsonFilterUtil.validateConfigs(filterConfig, instrumentation);
+    }
+
+    @Test
+    public void shouldThrowIllegalArgumentExceptionForNullMessageFormat() {
+        Map<String, String> filterConfigs = new HashMap<>();
+        filterConfigs.put("FILTER_JSON_DATA_SOURCE", "message");
+        filterConfigs.put("FILTER_JSON_SCHEMA", "{\"properties\":{\"order_number\":{\"const\":\"123\"}}}");
+        filterConfigs.put("FILTER_JSON_SCHEMA_PROTO_CLASS", TestMessage.class.getName());
+        filterConfig = ConfigFactory.create(FilterConfig.class, filterConfigs);
+        thrown.expect(IllegalArgumentException.class);
+        JsonFilterUtil.validateConfigs(filterConfig, instrumentation);
+    }
+
+    @Test
+    public void shouldThrowExceptionForNullProtoSchemaClassForProtobufMessageFormat() {
+        Map<String, String> filterConfigs = new HashMap<>();
+        filterConfigs.put("FILTER_JSON_DATA_SOURCE", "message");
+        filterConfigs.put("FILTER_ESB_MESSAGE_FORMAT", "PROTOBUF");
+        filterConfigs.put("FILTER_JSON_SCHEMA", "{\"properties\":{\"order_number\":{\"const\":\"123\"}}}");
+        filterConfig = ConfigFactory.create(FilterConfig.class, filterConfigs);
+        thrown.expect(IllegalArgumentException.class);
+        JsonFilterUtil.validateConfigs(filterConfig, instrumentation);
+    }
+
+    @Test
+    public void shouldNotThrowIllegalArgumentExceptionForValidFilterConfig() {
+        Map<String, String> filterConfigs = new HashMap<>();
+        filterConfigs.put("FILTER_JSON_DATA_SOURCE", "message");
+        filterConfigs.put("FILTER_ESB_MESSAGE_FORMAT", "PROTOBUF");
+        filterConfigs.put("FILTER_JSON_SCHEMA", "{\"properties\":{\"order_number\":{\"const\":\"123\"}}}");
+        filterConfigs.put("FILTER_JSON_SCHEMA_PROTO_CLASS", TestMessage.class.getName());
+        filterConfig = ConfigFactory.create(FilterConfig.class, filterConfigs);
+        JsonFilterUtil.validateConfigs(filterConfig, instrumentation);
     }
 }
