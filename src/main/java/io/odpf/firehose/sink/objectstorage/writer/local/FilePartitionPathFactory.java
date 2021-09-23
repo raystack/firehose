5a9f808586613a9671bd77e694034f117d93a6bd
lavkesh <lavkesh51@gmail.com>
2021-09-23 14:49:53 +0800

    refactor: moved methods to correct classes

--- src/main/java/io/odpf/firehose/sink/objectstorage/writer/local/FilePartitionPathFactory.java
+++ src/main/java/io/odpf/firehose/sink/objectstorage/writer/local/FilePartitionPathFactory.java
@@ -1,10 +1,7 @@
 package io.odpf.firehose.sink.objectstorage.writer.local;
 
-import com.google.protobuf.Descriptors;
-import com.google.protobuf.DynamicMessage;
 import io.odpf.firehose.sink.objectstorage.Constants;
 import io.odpf.firehose.sink.objectstorage.message.Record;
-import io.odpf.firehose.sink.objectstorage.proto.KafkaMetadataMessage;
 import lombok.AllArgsConstructor;
 
 import java.nio.file.Path;
@@ -16,8 +13,8 @@ import java.time.Instant;
 @AllArgsConstructor
 public class FilePartitionPathFactory {
 
-    private final String kafkaMetadataFieldName;
-    private final String fieldName;
+    private final String metadataFieldName;
+    private final String timeStampFieldName;
     private FilePartitionPathConfig filePartitionPathConfig;
 
     public Path getPartitionPath(Record record) {
@@ -26,13 +23,11 @@ public class FilePartitionPathFactory {
     }
 
     public FilePartitionPath getFilePartitionPath(Record record) {
-        DynamicMessage metadataMessage = record.getMetadata();
-        String topic = getTopic(metadataMessage);
+        String topic = record.getTopic(metadataFieldName);
 
         Instant timestamp = null;
         if (filePartitionPathConfig.getFilePartitionType() != Constants.FilePartitionType.NONE) {
-            DynamicMessage message = record.getMessage();
-            timestamp = getTimestamp(message);
+            timestamp = record.getTimestamp(timeStampFieldName);
         }
 
         return new FilePartitionPath(topic, timestamp, filePartitionPathConfig);
@@ -40,26 +35,5 @@ public class FilePartitionPathFactory {
 
     public FilePartitionPath fromFilePartitionPath(String filePartitionPath) {
         return FilePartitionPath.parseFrom(filePartitionPath, filePartitionPathConfig);
-    }
-
-    private String getTopic(DynamicMessage dynamicMessage) {
-        Descriptors.Descriptor metadataDescriptor = dynamicMessage.getDescriptorForType();
-
-        if (!kafkaMetadataFieldName.isEmpty()) {
-            DynamicMessage nestedMetadataMessage = (DynamicMessage) dynamicMessage.getField(metadataDescriptor.findFieldByName(kafkaMetadataFieldName));
-            Descriptors.Descriptor nestedMetadataMessageDescriptor = nestedMetadataMessage.getDescriptorForType();
-            return (String) nestedMetadataMessage.getField(nestedMetadataMessageDescriptor.findFieldByName(KafkaMetadataMessage.MESSAGE_TOPIC_FIELD_NAME));
-        }
-
-        return (String) dynamicMessage.getField(metadataDescriptor.findFieldByName(KafkaMetadataMessage.MESSAGE_TOPIC_FIELD_NAME));
-    }
-
-    private Instant getTimestamp(DynamicMessage dynamicMessage) {
-        Descriptors.Descriptor descriptor = dynamicMessage.getDescriptorForType();
-        Descriptors.FieldDescriptor timestampField = descriptor.findFieldByName(fieldName);
-        DynamicMessage timestamp = (DynamicMessage) dynamicMessage.getField(timestampField);
-        long seconds = (long) timestamp.getField(timestamp.getDescriptorForType().findFieldByName("seconds"));
-        int nanos = (int) timestamp.getField(timestamp.getDescriptorForType().findFieldByName("nanos"));
-        return Instant.ofEpochSecond(seconds, nanos);
     }
 }
