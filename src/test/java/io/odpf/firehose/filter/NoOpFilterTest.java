f5c82f07b891ae64457abdd9f70a139511b0474d
Sumit Aich <aich.1998@gmail.com>
2021-09-23 22:57:28 +0530

    test: add tests for NoOpFilter

--- src/test/java/io/odpf/firehose/filter/NoOpFilterTest.java
+++ src/test/java/io/odpf/firehose/filter/NoOpFilterTest.java
@@ -1,11 +1,18 @@
 package io.odpf.firehose.filter;
 
+import io.odpf.firehose.consumer.Message;
+import io.odpf.firehose.consumer.TestKey;
+import io.odpf.firehose.consumer.TestMessage;
 import io.odpf.firehose.metrics.Instrumentation;
 import org.junit.Before;
 import org.junit.Test;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 
+import java.util.Arrays;
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 
@@ -14,14 +21,30 @@ public class NoOpFilterTest {
     @Mock
     private Instrumentation instrumentation;
 
+    private NoOpFilter noOpFilter;
+
     @Before
     public void setup() {
         MockitoAnnotations.initMocks(this);
+        noOpFilter = new NoOpFilter(instrumentation);
     }
 
     @Test
-    public void shouldLogFilterTypeIfFilterTypeIsNone() {
+    public void shouldLogNoFilterSelected() {
         new NoOpFilter(instrumentation);
         verify(instrumentation, times(1)).logInfo("No filter is selected");
+    }
+
+    @Test
+    public void shouldReturnInputListOfMessages() throws FilterException {
+        TestKey testKeyProto1 = TestKey.newBuilder().setOrderNumber("123").setOrderUrl("abc").build();
+        TestMessage testMessageProto1 = TestMessage.newBuilder().setOrderNumber("123").setOrderUrl("abc").setOrderDetails("details").build();
+        TestKey testKeyProto2 = TestKey.newBuilder().setOrderNumber("92").setOrderUrl("pqr").build();
+        TestMessage testMessageProto2 = TestMessage.newBuilder().setOrderNumber("92").setOrderUrl("pqr").setOrderDetails("details").build();
+        Message message1 = new Message(testKeyProto1.toByteArray(), testMessageProto1.toByteArray(), "topic1", 0, 100);
+        Message message2 = new Message(testKeyProto2.toByteArray(), testMessageProto2.toByteArray(), "topic1", 0, 101);
+        List<Message> inputMessages = Arrays.asList(message1, message2);
+        List<Message> filteredMessages = noOpFilter.filter(Arrays.asList(message1, message2));
+        assertEquals(inputMessages, filteredMessages);
     }
 }
