21902fd2d130f1da5e2f3f856ac6c84aa01dd64e
lavkesh <lavkesh51@gmail.com>
2021-09-27 18:14:35 +0800

    fixed: DLQ message format

--- src/main/java/io/odpf/firehose/sinkdecorator/dlq/objectstorage/ObjectStorageDlqWriter.java
+++ src/main/java/io/odpf/firehose/sinkdecorator/dlq/objectstorage/ObjectStorageDlqWriter.java
@@ -1,12 +1,13 @@
 package io.odpf.firehose.sinkdecorator.dlq.objectstorage;
 
-import com.google.gson.FieldNamingPolicy;
-import com.google.gson.Gson;
-import io.odpf.firehose.error.ErrorInfo;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import io.odpf.firehose.consumer.Message;
 import io.odpf.firehose.objectstorage.ObjectStorageException;
 import io.odpf.firehose.objectstorage.ObjectStorage;
 import io.odpf.firehose.sinkdecorator.dlq.DlqWriter;
+import lombok.extern.slf4j.Slf4j;
+import org.bson.internal.Base64;
 
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
@@ -22,77 +23,53 @@ import java.util.Map;
 import java.util.UUID;
 import java.util.stream.Collectors;
 
+@Slf4j
 public class ObjectStorageDlqWriter implements DlqWriter {
-
     private final ObjectStorage objectStorage;
-    private final Gson gson;
+    private final ObjectMapper objectMapper;
 
     public ObjectStorageDlqWriter(ObjectStorage objectStorage) {
         this.objectStorage = objectStorage;
-        gson = new Gson().newBuilder()
-                .setFieldNamingPolicy(FieldNamingPolicy.LOWER_CASE_WITH_UNDERSCORES)
-                .create();
+        this.objectMapper = new ObjectMapper();
     }
 
     @Override
     public List<Message> write(List<Message> messages) throws IOException {
-        Map<Path, List<Message>> messagesByPartition = groupByPartition(messages);
-
-        List<Message> unProcessedMessages = new LinkedList<>();
-        for (Map.Entry<Path, List<Message>> entry : messagesByPartition.entrySet()) {
-            List<DlqMessage> dlqMessages = createDlqMessages(entry.getValue());
-            String dlqJson = serialise(dlqMessages);
-
+        Map<Path, List<Message>> messagesByPartition = messages.stream().collect(Collectors.groupingBy(this::createPartition));
+        List<Message> failedMessages = new LinkedList<>();
+        messagesByPartition.forEach((path, partitionedMessages) -> {
+            String data = partitionedMessages.stream().map(this::convertToString).collect(Collectors.joining("\n"));
             String fileName = UUID.randomUUID().toString();
-            String objectName = entry.getKey().resolve(fileName).toString();
-
+            String objectName = path.resolve(fileName).toString();
             try {
-                objectStorage.store(objectName, dlqJson.getBytes(StandardCharsets.UTF_8));
+                objectStorage.store(objectName, data.getBytes(StandardCharsets.UTF_8));
             } catch (ObjectStorageException e) {
-                throw new IOException(e);
+                log.warn("Not able to store into DLQ messages into object storage", e);
+                failedMessages.addAll(partitionedMessages);
             }
-        }
-
-        return unProcessedMessages;
+        });
+        return failedMessages;
     }
 
-    private Map<Path, List<Message>> groupByPartition(List<Message> messages) {
-        return messages.stream()
-                .collect(Collectors.groupingBy(ObjectStorageDlqWriter::createPartition));
-    }
-
-    private List<DlqMessage> createDlqMessages(List<Message> messages) {
-        List<DlqMessage> dlqMessages = messages.stream().map(message -> {
-            String error = "";
-            ErrorInfo errorInfo = message.getErrorInfo();
-            if (errorInfo != null) {
-                if (errorInfo.getErrorType() != null) {
-                    error = errorInfo.getErrorType().toString();
-                }
-            }
-
-            return new DlqMessage(new String(message.getLogKey()),
-                    new String(message.getLogMessage()),
+    private String convertToString(Message message) {
+        try {
+            return objectMapper.writeValueAsString(new DlqMessage(
+                    Base64.encode(message.getLogKey()),
+                    Base64.encode(message.getLogMessage()),
                     message.getTopic(),
                     message.getPartition(),
                     message.getOffset(),
-                    message.getConsumeTimestamp(),
-                    error);
-        }).collect(Collectors.toList());
-        return dlqMessages;
+                    message.getTimestamp(),
+                    message.getErrorInfo().toString()));
+        } catch (JsonProcessingException e) {
+            log.warn("Not able to convert message into json", e);
+            return "";
+        }
     }
 
-    public String serialise(List<DlqMessage> messages) {
-        List<String> jsonMessages = messages.stream()
-                .map(gson::toJson)
-                .collect(Collectors.toList());
-        return String.join("\n", jsonMessages);
-    }
-
-    private static Path createPartition(Message message) {
+    private Path createPartition(Message message) {
         LocalDate consumeLocalDate = LocalDate.from(Instant.ofEpochMilli(message.getConsumeTimestamp())
                 .atZone(ZoneId.of("UTC")));
-
         String consumeDate = DateTimeFormatter.ISO_LOCAL_DATE.format(consumeLocalDate);
         return Paths.get(message.getTopic(), consumeDate);
     }
