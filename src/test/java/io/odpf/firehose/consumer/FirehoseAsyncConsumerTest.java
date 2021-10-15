3cba27d714a5cd8247397fb310d32f1729e47a2a
lavkesh <lavkesh51@gmail.com>
2021-10-15 23:02:35 +0800

    tests: filter test in firehose consumer

--- src/test/java/io/odpf/firehose/consumer/FirehoseAsyncConsumerTest.java
+++ src/test/java/io/odpf/firehose/consumer/FirehoseAsyncConsumerTest.java
@@ -1,8 +1,10 @@
 package io.odpf.firehose.consumer;
 
 import io.odpf.firehose.filter.FilterException;
+import io.odpf.firehose.filter.FilteredMessages;
 import io.odpf.firehose.filter.NoOpFilter;
 import io.odpf.firehose.metrics.Instrumentation;
+import io.odpf.firehose.metrics.Metrics;
 import io.odpf.firehose.tracer.SinkTracer;
 import org.junit.Before;
 import org.junit.Rule;
@@ -12,6 +14,7 @@ import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
 
+import java.time.Instant;
 import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
@@ -86,6 +89,8 @@ public class FirehoseAsyncConsumerTest {
 
         Mockito.verify(consumerAndOffsetManager, Mockito.times(1)).addOffsets(future1, messages);
         Mockito.verify(consumerAndOffsetManager, Mockito.times(1)).setCommittable(future1);
+        Mockito.verify(consumerAndOffsetManager, Mockito.times(1)).addOffsetsAndSetCommittable(new ArrayList<>());
+        Mockito.verify(consumerAndOffsetManager, Mockito.times(1)).commit();
     }
 
     @Test
@@ -100,5 +105,42 @@ public class FirehoseAsyncConsumerTest {
         Mockito.when(sinkPool.submitTask(messages)).thenReturn(future1);
         Mockito.when(sinkPool.fetchFinishedSinkTasks()).thenThrow(new AsyncConsumerFailedException(new RuntimeException()));
         asyncConsumer.process();
+    }
+
+    @Test
+    public void shouldAddOffsetsForFilteredMessages() throws Exception {
+        FirehoseFilter firehoseFilter = Mockito.mock(FirehoseFilter.class);
+        this.asyncConsumer = new FirehoseAsyncConsumer(sinkPool, tracer, consumerAndOffsetManager, firehoseFilter, instrumentation);
+
+        List<Message> messages = new ArrayList<Message>() {{
+            add(new Message(new byte[0], new byte[0], "topic1", 1, 10));
+            add(new Message(new byte[0], new byte[0], "topic1", 1, 11));
+            add(new Message(new byte[0], new byte[0], "topic1", 1, 12));
+        }};
+
+        Mockito.when(consumerAndOffsetManager.readMessagesFromKafka()).thenReturn(messages);
+        Mockito.when(firehoseFilter.applyFilter(messages)).thenReturn(new FilteredMessages() {{
+            addToValidMessages(messages.get(0));
+            addToInvalidMessages(messages.get(1));
+            addToInvalidMessages(messages.get(2));
+        }});
+        Mockito.when(sinkPool.submitTask(new ArrayList<Message>() {{
+            add(messages.get(0));
+        }})).thenReturn(future1);
+        Mockito.when(sinkPool.fetchFinishedSinkTasks()).thenReturn(new HashSet<Future<List<Message>>>() {{
+            add(future1);
+        }});
+        asyncConsumer.process();
+
+        Mockito.verify(consumerAndOffsetManager, Mockito.times(1)).addOffsets(future1, new ArrayList<Message>() {{
+            add(messages.get(0));
+        }});
+        Mockito.verify(consumerAndOffsetManager, Mockito.times(1)).addOffsetsAndSetCommittable(new ArrayList<Message>() {{
+            add(messages.get(1));
+            add(messages.get(2));
+        }});
+        Mockito.verify(consumerAndOffsetManager, Mockito.times(1)).setCommittable(future1);
+        Mockito.verify(consumerAndOffsetManager, Mockito.times(1)).commit();
+        Mockito.verify(instrumentation, Mockito.times(1)).captureDurationSince(Mockito.eq(Metrics.SOURCE_KAFKA_PARTITIONS_PROCESS_TIME_MILLISECONDS), Mockito.any(Instant.class));
     }
 }
