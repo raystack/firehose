c70851862669bc0fbb6c6466dd627bfe080df287
kevin.bheda <kevin.bheda@go-jek.com>
2022-06-08 17:29:15 +0530

    feat: json support for bq sink

--- src/main/java/io/odpf/firehose/sink/SinkFactoryUtils.java
+++ src/main/java/io/odpf/firehose/sink/SinkFactoryUtils.java
@@ -10,6 +10,10 @@ public class SinkFactoryUtils {
         Map<String, String> finalConfig = new HashMap<>(env);
         finalConfig.put("SINK_CONNECTOR_SCHEMA_MESSAGE_CLASS", env.getOrDefault("INPUT_SCHEMA_PROTO_CLASS", ""));
         finalConfig.put("SINK_CONNECTOR_SCHEMA_KEY_CLASS", env.getOrDefault("INPUT_SCHEMA_PROTO_CLASS", ""));
+        //TODO discuss with folks if we should do env.get or appconfig.getConfigName
+        finalConfig.put("SINK_CONNECTOR_SCHEMA_DATA_TYPE", env.getOrDefault("INPUT_DATA_FORMAT", "protobuf"));
+        finalConfig.put("SINK_CONNECTOR_SCHEMA_JSON_DYNAMIC_SCHEMA_ENABLE", env.getOrDefault("INPUT_JSON_DYNAMIC_SCHEMA_ENABLE", "true"));
+        finalConfig.put("SINK_CONNECTOR_SCHEMA_JSON_OUTPUT_DEFAULT_DATATYPE_STRING_ENABLE", env.getOrDefault("INPUT_JSON_OUTPUT_DEFAULT_DATATYPE_STRING_ENABLE", "true"));
         finalConfig.put("SINK_METRICS_APPLICATION_PREFIX", "firehose_");
         finalConfig.put("SINK_CONNECTOR_SCHEMA_PROTO_ALLOW_UNKNOWN_FIELDS_ENABLE", env.getOrDefault("INPUT_SCHEMA_PROTO_ALLOW_UNKNOWN_FIELDS_ENABLE", "false"));
         finalConfig.put("SINK_CONNECTOR_SCHEMA_MESSAGE_MODE",
