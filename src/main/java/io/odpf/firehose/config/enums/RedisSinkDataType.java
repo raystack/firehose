cd373f3eae19d68beb0f76bd67fd7d3304397eba
Sumit Aich <aich.1998@gmail.com>
2022-12-06 14:13:54 +0530

    feat: redis sink using depot library (#206)
    * feat: redis sink using depot library
    
    * docs: redis sink using depot library
    
    * chore: version bump
    
    * fix: remove descriptors
    
    * chore: version bump
    
    * chore: version bump

--- src/main/java/io/odpf/firehose/config/enums/RedisSinkDataType.java
+++
@@ -1,7 +0,0 @@
-package io.odpf.firehose.config.enums;
-
-public enum RedisSinkDataType {
-    LIST,
-    HASHSET,
-    KEYVALUE,
-}
--- src/main/java/io/odpf/firehose/sink/SinkFactory.java
+++ src/main/java/io/odpf/firehose/sink/SinkFactory.java
@@ -3,12 +3,15 @@ package io.odpf.firehose.sink;
 import io.odpf.depot.bigquery.BigQuerySink;
 import io.odpf.depot.bigquery.BigQuerySinkFactory;
 import io.odpf.depot.config.BigQuerySinkConfig;
+import io.odpf.depot.config.RedisSinkConfig;
 import io.odpf.depot.bigtable.BigTableSinkFactory;
 import io.odpf.depot.bigtable.BigTableSink;
 import io.odpf.depot.config.BigTableSinkConfig;
 import io.odpf.depot.log.LogSink;
 import io.odpf.depot.log.LogSinkFactory;
 import io.odpf.depot.metrics.StatsDReporter;
+import io.odpf.depot.redis.RedisSink;
+import io.odpf.depot.redis.RedisSinkFactory;
 import io.odpf.firehose.config.KafkaConsumerConfig;
 import io.odpf.firehose.config.enums.SinkType;
 import io.odpf.firehose.consumer.kafka.OffsetManager;
@@ -23,7 +26,6 @@ import io.odpf.firehose.sink.influxdb.InfluxSinkFactory;
 import io.odpf.firehose.sink.jdbc.JdbcSinkFactory;
 import io.odpf.firehose.sink.mongodb.MongoSinkFactory;
 import io.odpf.firehose.sink.prometheus.PromSinkFactory;
-import io.odpf.firehose.sink.redis.RedisSinkFactory;
 import io.odpf.stencil.client.StencilClient;
 import org.aeonbits.owner.ConfigFactory;
 
@@ -38,6 +40,7 @@ public class SinkFactory {
     private BigQuerySinkFactory bigQuerySinkFactory;
     private BigTableSinkFactory bigTableSinkFactory;
     private LogSinkFactory logSinkFactory;
+    private RedisSinkFactory redisSinkFactory;
     private final Map<String, String> config;
 
     public SinkFactory(KafkaConsumerConfig kafkaConsumerConfig,
@@ -61,7 +64,6 @@ public class SinkFactory {
             case HTTP:
             case INFLUXDB:
             case ELASTICSEARCH:
-            case REDIS:
             case GRPC:
             case PROMETHEUS:
             case BLOB:
@@ -70,6 +72,12 @@ public class SinkFactory {
             case LOG:
                 logSinkFactory = new LogSinkFactory(config, statsDReporter);
                 logSinkFactory.init();
+                return;
+            case REDIS:
+                redisSinkFactory = new RedisSinkFactory(
+                        ConfigFactory.create(RedisSinkConfig.class, config),
+                        statsDReporter);
+                redisSinkFactory.init();
                 return;
             case BIGQUERY:
                 BigquerySinkUtils.addMetadataColumns(config);
@@ -105,7 +113,7 @@ public class SinkFactory {
             case ELASTICSEARCH:
                 return EsSinkFactory.create(config, statsDReporter, stencilClient);
             case REDIS:
-                return RedisSinkFactory.create(config, statsDReporter, stencilClient);
+                return new GenericOdpfSink(new FirehoseInstrumentation(statsDReporter, RedisSink.class), sinkType.name(), redisSinkFactory.create());
             case GRPC:
                 return GrpcSinkFactory.create(config, statsDReporter, stencilClient);
             case PROMETHEUS:
